14f037069fd8b2d107c902b0697b0ac0d7d5b519
Alberto Torres <albertotb@gmail.com>
2021-06-02 12:30:38 +0200

    set initial version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="template",
-    version="0.0.0",
+    version="0.1.0",
     packages=find_packages(),
     python_requires=">=3.9",
     setup_requires=["setuptools_scm"],
