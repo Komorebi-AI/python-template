399263eeb553614673d4482a6622a730e852bdcd
Alberto Torres <albertotb@gmail.com>
2023-09-19 16:29:33 +0000

    añade api y Dockerfile

---
+++ template/api.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from template.main import __version__
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"template-api": f"version {__version__}"}
+
+
+if __name__ == "__main__":
+    import sys
+
+    import uvicorn
+
+    port = 7654 if len(sys.argv) < 2 else int(sys.argv[1])
+    host = "127.0.0.1" if len(sys.argv) < 3 else sys.argv[2]
+    uvicorn.run("api:app", host=host, port=port, reload=True)
