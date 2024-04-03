b33746278e3dd2e964d8513122771e4ecf3df24e
Alberto Torres <albertotb@gmail.com>
2024-04-03 18:38:05 +0200

    make compatible with 3.9

--- template/api.py
+++ template/api.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from fastapi import FastAPI
 
 from template.main import __version__
--- template/main.py
+++ template/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import annotations
+
 from importlib.metadata import PackageNotFoundError, version
 
 import typer
